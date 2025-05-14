e51180a0527958dd39d635cf70e6bc65bb6eb8b4
themahmoudzaki <Mahmoudzakylinkedin@gmail.com>
2025-05-14 16:19:42 +0200

    API Created

--- computer-vision/preprocess_D1.py
+++ computer-vision/preprocess_D1.py
@@ -1,6 +1,7 @@
 from utils import *
 
-def preprocess_img(img_array):
-  arr_scaled = tf.cast(img_array, tf.float32) / 255.0
 
-  return arr_scaled
+def preprocess_img(img_array):
+    arr_scaled = tf.cast(img_array, tf.float32) / 255.0
+
+    return arr_scaled
--- computer-vision/utils.py
+++ computer-vision/utils.py
@@ -1,37 +1,40 @@
 import os
-import pandas             as pd
-import numpy              as np
-import matplotlib.pyplot  as plt
-import tensorflow         as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
 import math
+import uvicorn
+import cv2
 
-from pathlib                  import Path
-from sklearn.model_selection  import train_test_split
-from sklearn.metrics          import confusion_matrix, classification_report
-from concurrent.futures       import ThreadPoolExecutor
-from tqdm                     import tqdm
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+from tensorflow.keras.models import load_model
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
-
-BASE_DIR      = Path('Data')
+BASE_DIR = Path("Data")
 
 # Tensorflow
-GPU_S = tf.config.list_physical_devices('GPU')
-EPOCHS      = 100
-BATCH_SIZE  = 32
-IMAGE_SIZE  = (224, 224)
-BATCH_SIZE  = 32
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+GPU_S = tf.config.list_physical_devices("GPU")
+IMAGE_SIZE = (224, 224)
+EPOCHS = 100
+BATCH_SIZE = 64
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
 # D1
-DATASET_1     = BASE_DIR      / 'dataset_1'
-BEE_DIR       = DATASET_1     / 'bee'
-NON_BEE_DIR   = DATASET_1     / 'non_bee'
-MIMICS_DIR    = DATASET_1     / 'mimics'
-MODEL_D1_DIR  = Path('model')
-MODEL_D1_FILE = MODEL_D1_DIR  / 'D1'
+DATASET_1 = BASE_DIR / "dataset_1"
+BEE_DIR = DATASET_1 / "bee"
+NON_BEE_DIR = DATASET_1 / "non_bee"
+MIMICS_DIR = DATASET_1 / "mimics"
+MODEL_D1_DIR = Path("model")
+MODEL_D1_FILE = MODEL_D1_DIR / "D1.h5"
+
 
 def section_print(str_s):
-    print('\n\n')
-    print(f'------------------- {str_s} -------------------')
-    print('\n')
+    print("\n\n")
+    print(f"------------------- {str_s} -------------------")
+    print("\n")
