9de19a506247d76efc91818c84034918d4087fc4
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2025-05-15 09:21:05 +0300

    rename app directory to mobileapp for clarity

---
+++ mobileapp/components/HapticTab.tsx
@@ -0,0 +1,18 @@
+import { BottomTabBarButtonProps } from "@/node_modules/@react-navigation/bottom-tabs/lib/typescript/src";
+import { PlatformPressable } from "@react-navigation/elements";
+import * as Haptics from "expo-haptics";
+
+export function HapticTab(props: BottomTabBarButtonProps) {
+	return (
+		<PlatformPressable
+			{...props}
+			onPressIn={(ev) => {
+				if (process.env.EXPO_OS === "ios") {
+					// Add a soft haptic feedback when pressing down on the tabs.
+					Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+				}
+				props.onPressIn?.(ev);
+			}}
+		/>
+	);
+}
